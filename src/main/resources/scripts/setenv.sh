655154e17ceb514a611ae0c455a2616ed8ee131c
nrapopor <nrapopor@hotmail.com>
2017-09-05 16:19:34 -0400

    Fixed the .gitignore
    /data -- This directory will be updated by the build

--- src/main/resources/scripts/setenv.sh
+++ src/main/resources/scripts/setenv.sh
@@ -1,9 +1,10 @@
 	# Required for maven 2
-export  M3=
-export  M3_HOME=
-export  M2=/usr/local/lib/apache-maven-2.2.1
+#export  M2=/usr/local/lib/apache-maven-2.2.1
+export  M2=/usr/local/maven
 export  M2_HOME=$M2
-export  JAVA_HOME=/usr/local/java/jdk1.6.0_24
+export  M3=$M2
+export  M3_HOME=$M2
+export  JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export  PATH=$M2/bin:$JAVA_HOME/bin:$PATH
 export  MVN_OPTS=-ea
 # required for the deploy-file goal
